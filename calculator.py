1e6cb8305c5c6b0ef434cc21ddd7ff327b02d024
Redemptor <namiremberedemptor@gmail.com>
2026-04-05 15:22:57 +0300

    Add get_number() test

---
+++ calculator.py
@@ -0,0 +1,10 @@
+"""
+A simple calculator that performs addition and multiplication operations.
+The calculator takes two numbers as input and allows the user to choose the
+desired operation. The result is then displayed to the user.
+"""
+
+
+def get_numbers():
+    """Get numbers from user input."""
+    pass
